a916d98841bc2cfe140326f2b52dccc173044d22
EddieOMan <edward_offord@yahoo.co.uk>
2016-04-06 20:20:45 +0100

    Update RSA-Encryption.py

--- RSA-Encryption.py
+++ RSA-Encryption.py
@@ -2,14 +2,15 @@
 import re
 import random
 
-dataIn = "".join([chr(random.randint(0,220)) for x in range(100000)])#input()
+##dataIn = "".join([chr(random.randint(65,90)) for x in range(100000)])#input()
+dataIn = input()
 dataInList = list(dataIn)
 
-pubKey = 13*17
-sharedKey = 5
+pubKey = 4363*2539
+sharedKey = 17
 encry = []
 
-privateKey = 77
+privateKey = 651221
 for m in dataInList:
     message = ord(m)
     encry.append(hex((message**sharedKey)%pubKey))
@@ -20,19 +21,19 @@ encry = "".join(encry)
 #-----------------------DATA TRANSFER-------------#
 
 #-----------------------Intercept-----------------#
-intercept = []
-encry = encry.split("0x")[:0:-1][::-1]
-for x in encry:
-    intercept.append(chr(int(x,16)))
-
-#print("".join(intercept))
+##intercept = []
+encry = encry.split("0x")[1:]
+##for x in encry:
+##    intercept.append(chr(int(x,16)))
+##
+##print("".join(intercept))
 #-----------------------End-----------------------#
 dataOut = []
 ##encry = re.findall("....", encry)
 for c in encry:
+
     dataOut.append(chr((int(c,16)**privateKey)%pubKey))
 
-#print("".join(dataOut))
+print("".join(dataOut))
 
 if "".join(dataOut) == dataIn:print("Good")
-else:print("".join(dataOut),"".join(dataIn))
